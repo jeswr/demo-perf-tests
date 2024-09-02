b6b90c822d56d49e37e0c71841a76ef04cdfbc83
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-09-02 12:41:47 +1000

    feat/browser test (#57)
    * feat: browser-test
    
    * chore: remove lint pre commit hook
    
    * chore: use latest eyereasoner

---
+++ browser.js
@@ -0,0 +1,23 @@
+const webdriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/chrome');
+const path = require('path');
+const options = new firefox.Options();
+options.addArguments('headless');
+options.addArguments('disable-gpu'); // Optional, but recommended for headless mode
+const prefs = new webdriver.logging.Preferences();
+prefs.setLevel(webdriver.logging.Type.BROWSER, webdriver.logging.Level.ALL);
+options.setLoggingPrefs(prefs)
+
+const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+
+(async function(){
+    const filePath = `file://${path.join(__dirname, 'webpage.html')}`;
+    await driver.get(filePath);
+    await driver.sleep(2000);
+
+    const logs = await driver.manage().logs().get(webdriver.logging.Type.BROWSER);
+    logs.forEach(log => console.log(log.message));
+})();
